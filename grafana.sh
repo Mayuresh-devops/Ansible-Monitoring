cdbac277f906f67bca3a85c0dc50e97e63c21d73
Mayuresh <mayureshsk11@gmail.com>
2024-09-14 21:54:16 +0530

    --- Auto Git Commit ---

--- grafana.sh
+++ grafana.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 wget https://dl.grafana.com/oss/release/grafana-11.1.3.linux-amd64.tar.gz
 tar -zxvf grafana-11.1.3.linux-amd64.tar.gz
+sudo cp prometheus-2.53.1.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-2.53.1.linux-amd64/promtool /usr/local/bin/
