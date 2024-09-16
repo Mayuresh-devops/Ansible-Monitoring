1fa59a34033cf9accfea471c28887aaf3558bbf6
Mayuresh <mayureshsk11@gmail.com>
2024-09-16 14:40:11 +0530

    --- Auto Git Commit ---

--- grafana.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://dl.grafana.com/oss/release/grafana-11.1.3.linux-amd64.tar.gz
-tar -zxvf grafana-11.1.3.linux-amd64.tar.gz
-sudo cp /home/ubuntu/grafana-v11.1.3/bin/grafana-server /usr/local/bin/
